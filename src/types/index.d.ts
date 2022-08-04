fd917c4bb218806d5275403a215d7de6eb6f778e
yuyang <uyarnchen@gmail.com>
2022-08-04 11:14:19 +0800

    chore: fix security vulnerabilities (#28)

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,5 @@
-declare module 'consola';
 declare module 'fs-extra';
 declare module 'ora';
-declare module 'tracer';
-declare module 'git-clone';
 declare module 'clear';
 declare module 'figlet';
 declare module 'download-git-repo';
