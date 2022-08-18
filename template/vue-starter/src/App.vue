f217f321a5e2925a15132d65bfdcda4f5ff955fb
williamliao <jamesliauw@163.com>
2022-08-18 19:53:18 +0800

    feat: 添加最简结构

--- template/vue-starter/src/App.vue
+++ template/vue-starter/src/App.vue
@@ -1,19 +1,40 @@
 <template>
-  <router-view :class="[mode]" />
+  <div> <t-button theme="primary">
+      <add-icon slot="icon"/>
+      欢迎
+    </t-button></div>
 </template>
 
 <script>
 import Vue from 'vue';
-import config from '@/config/style';
 
 export default Vue.extend({
-  computed: {
-    mode() {
-      return this.$store.getters['setting/mode'];
-    },
-  },
   mounted() {
-    this.$store.dispatch('setting/changeTheme', { ...config });
+    // hook
   },
 });
 </script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
