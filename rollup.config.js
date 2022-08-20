a2f61a590cfa51474d78a5cb7926bbc7e2bd0fbb
williamliao <jamesliauw@163.com>
2022-08-20 12:25:47 +0800

    feat: add copy plugin

--- rollup.config.js
+++ rollup.config.js
@@ -7,50 +7,57 @@ import { terser } from 'rollup-plugin-terser';
 import typescript2 from 'rollup-plugin-typescript2';
 import rollupResolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
+import copy from 'rollup-plugin-copy';
 
 const extensions = ['.js', '.ts'];
 
 const external = Object.keys(pkg.dependencies || '');
 const globals = external.reduce((prev, current) => {
-	const newPrev = prev;
-	newPrev[current] = current;
-	return newPrev;
+    const newPrev = prev;
+    newPrev[current] = current;
+    return newPrev;
 }, {});
 
 const defaultConfig = {
-	input: pkg.main,
-	output: {
-		file: path.resolve(__dirname, pkg.lib),
-		format: 'cjs',
-		banner: '#!/usr/bin/env node',
-		globals
-	},
-	external,
-	plugins: [
-		typescript2({
-			exclude: 'node_modules/**',
-			useTsconfigDeclarationDir: true,
-			typescript,
-			tsconfig: './tsconfig.json'
-		}),
-		json(),
-		terser(),
-		rollupResolve({
-			// 查找和打包node_modules中的第三方模块
-			customResolveOptions: {
-				moduleDirectory: 'src'
-			},
-			preferBuiltins: true,
-		}),
-		nodeResolve({
-			extensions,
-			modulesOnly: true
-		}),
-		babel({
-			exclude: 'node_modules/**',
-			extensions
-		})
-	]
+    input: pkg.main,
+    output: {
+        file: path.resolve(__dirname, pkg.lib),
+        format: 'cjs',
+        banner: '#!/usr/bin/env node',
+        globals
+    },
+    external,
+    plugins: [
+        typescript2({
+            exclude: 'node_modules/**',
+            useTsconfigDeclarationDir: true,
+            typescript,
+            tsconfig: './tsconfig.json'
+        }),
+        json(),
+        terser(),
+        rollupResolve({
+            // 查找和打包node_modules中的第三方模块
+            customResolveOptions: {
+                moduleDirectory: 'src'
+            },
+            preferBuiltins: true,
+        }),
+        nodeResolve({
+            extensions,
+            modulesOnly: true
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            extensions
+        }),
+        copy({
+            targets: [
+                { src: 'template/**/*', dest: 'bin/template' }
+            ],
+            verbose: true
+        })
+    ]
 };
 
 export default defaultConfig;
