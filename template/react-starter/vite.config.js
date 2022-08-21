09373363d4678f11b699da4daa0e9fc5c274289e
williamliao <jamesliauw@163.com>
2022-08-21 17:39:42 +0800

    feat: add simple config

--- template/react-starter/vite.config.js
+++ template/react-starter/vite.config.js
@@ -6,18 +6,18 @@ export default (params) => ({
     base: './',
     resolve: {
         alias: {
-            assets: path.resolve(__dirname, './src/assets'),
-            components: path.resolve(__dirname, './src/components'),
-            configs: path.resolve(__dirname, './src/configs'),
-            layouts: path.resolve(__dirname, './src/layouts'),
-            modules: path.resolve(__dirname, './src/modules'),
-            pages: path.resolve(__dirname, './src/pages'),
-            styles: path.resolve(__dirname, './src/styles'),
-            utils: path.resolve(__dirname, './src/utils'),
-            services: path.resolve(__dirname, './src/services'),
-            router: path.resolve(__dirname, './src/router'),
-            hooks: path.resolve(__dirname, './src/hooks'),
-            types: path.resolve(__dirname, './src/types'),
+            // assets: path.resolve(__dirname, './src/assets'),
+            // components: path.resolve(__dirname, './src/components'),
+            // configs: path.resolve(__dirname, './src/configs'),
+            // layouts: path.resolve(__dirname, './src/layouts'),
+            // modules: path.resolve(__dirname, './src/modules'),
+            // pages: path.resolve(__dirname, './src/pages'),
+            // styles: path.resolve(__dirname, './src/styles'),
+            // utils: path.resolve(__dirname, './src/utils'),
+            // services: path.resolve(__dirname, './src/services'),
+            // router: path.resolve(__dirname, './src/router'),
+            // hooks: path.resolve(__dirname, './src/hooks'),
+            // types: path.resolve(__dirname, './src/types'),
         },
     },
 
@@ -38,18 +38,5 @@ export default (params) => ({
 
     build: {
         cssCodeSplit: false,
-    },
-
-    server: {
-        host: '0.0.0.0',
-        port: 3003,
-        proxy: {
-            '/api': {
-                // 用于开发环境下的转发请求
-                // 更多请参考：https://vitejs.dev/config/#server-proxy
-                target: 'https://service-exndqyuk-1257786608.gz.apigw.tencentcs.com',
-                changeOrigin: true,
-            },
-        },
-    },
+    }
 });
