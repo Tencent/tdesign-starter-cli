113c943f6cc376ddf971cfd6b3ba32dac8c423f1
Uyarn <uyarnchen@gmail.com>
2022-08-22 20:27:35 +0800

    chore: optimize code

--- src/core/core-lite/CoreLiteInquirer.ts
+++ src/core/core-lite/CoreLiteInquirer.ts
@@ -19,7 +19,7 @@ export class CoreLiteInquirer extends CoreInquirer {
         name: 'type',
         message: '生成代码版本：',
         choices: [
-          { name: ' Lite版本 (只包含初始化及基本使用)', value: 'lite' },
+          { name: ' Lite版本 (只包含TDesign的基本使用和项目工程)', value: 'lite' },
           { name: ' 通用模板版本', value: 'custom' }
         ],
         default: 'lite'
