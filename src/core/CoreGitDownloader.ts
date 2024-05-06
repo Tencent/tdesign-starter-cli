92b50f555c855d80689f1aedc1b8d06a74750fe3
任帅 <1064425721@qq.com>
2024-05-06 14:20:41 +0800

    格式化代码 修复 build 错误问题

--- src/core/CoreGitDownloader.ts
+++ src/core/CoreGitDownloader.ts
@@ -8,7 +8,7 @@ import { CoreOptionsFilterForVue2, IOptionsFilter } from './core-options/CoreOpt
 import { CoreOptionsFilterForVue3 } from './core-options/CoreOptionsFilterForVue3';
 import { CoreOptionsFilterForReact } from './core-options/CoreOptionsFilterForReact';
 // import { CoreJsTransformer } from './core-js-transform/CoreJsTransformer';
-import{sync} from 'rimraf';
+import { sync } from 'rimraf';
 
 export class CoreGitDownloader {
   /**
