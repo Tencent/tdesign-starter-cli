63ea036b0cc95071db648f0be55847c9a5ed9c11
阿菜 Cai <1064425721@qq.com>
2024-05-23 14:52:11 +0800

    feat(template): add `farm` lite template (#61)
    * feat(template): add `farm` template
    
    * update`index.html`

---
+++ templates/farm/vue-lite/README.md
@@ -0,0 +1,45 @@
+<p style="display:flex; justify-content: center">
+
+</p>
+<p align="center">
+  <a href="https://tdesign.tencent.com/vue/overview" target="_blank">
+<img alt="TDesign Logo" width="200" src="./public/tdesign-logo.svg"/>
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://nodejs.org/en/about/releases/"><img src="https://img.shields.io/node/v/@farmfe/core.svg" alt="node compatility"></a>
+  <a href="https://github.com/Tencent/tdesign-react-starter/blob/develop/LICENSE">
+    <img src="https://img.shields.io/npm/l/tdesign-react.svg?sanitize=true" alt="License">
+  </a>
+</p>
+
+## 项目简介
+
+`tdesign-vue` 是一个 TDesign 适配桌面端的组件库，适合在 vue 2 技术栈项目中使用。
+
+## 开发
+
+### 安装依赖
+
+```bash
+npm install
+```
+
+### 启动项目
+
+```bash
+npm run dev
+```
+
+## 构建
+
+### 构建正式环境
+
+```bash
+npm run build
+```
+
+## 开源协议
+
+TDesign 遵循 [MIT 协议](https://github.com/Tencent/tdesign-starter-cli/blob/develop/LICENSE)。
---
+++ templates/farm/vue-lite/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <t-space direction="vertical" style="width: 100%; text-align: center">
+    <t-space>
+      <a href="https://tdesign.tencent.com/vue/overview" target="_blank">
+        <img src="../public/tdesign-logo.svg" class="logo tdesign" alt="TDesign" />
+      </a>
+      <a href="https://www.farmfe.org/" target="_blank">
+        <img src="./assets/svg/farm-logo.png" class="logo farm" alt="Farm" />
+      </a>
+    </t-space>
+    <h2> Welcome to use
+      <t-link size="large" theme="primary" href="https://tdesign.tencent.com/vue/overview" target="_blank">
+        <jump-icon slot="suffixIcon" />
+        TDesign-vue
+      </t-link>
+      +
+      <t-link size="large" theme="primary" href="https://www.farmfe.org/" target="_blank">
+        <jump-icon slot="suffixIcon" />
+        Farm
+      </t-link>
+      ！
+    </h2>
+    <h3>
+      Experience it quickly using the
+      <t-link theme="warning" href="https://tdesign.tencent.com/starter/vue/" target="_blank">
+        <jump-icon slot="suffixIcon" />
+        TDesign Starter
+      </t-link>
+      page template.
+    </h3>
+
+    <t-space>
+      <t-button theme="primary">
+        <add-icon slot="icon" />
+        新建
+      </t-button>
+      <t-button variant="outline">
+        <cloud-upload-icon slot="icon" />
+        上传文件
+      </t-button>
+      <t-button shape="circle" theme="primary">
+        <discount-icon slot="icon" />
+      </t-button>
+      <t-button shape="circle" theme="primary">
+        <cloud-download-icon slot="icon" />
+      </t-button>
+      <t-button variant="outline"> 搜索 </t-button>
+    </t-space>
+  </t-space>
+</template>
+
+<script>
+import Vue from "vue";
+import {
+  AddIcon,
+  CloudUploadIcon,
+  DiscountIcon,
+  CloudDownloadIcon,
+  JumpIcon
+} from "tdesign-icons-vue";
+
+export default Vue.extend({
+  components: {
+    AddIcon,
+    CloudUploadIcon,
+    DiscountIcon,
+    CloudDownloadIcon,
+    JumpIcon
+  },
+});
+</script>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 3em 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #194de9);
+}
+
+.logo.farm:hover {
+  filter: drop-shadow(0 0 2em #bd34fe);
+}
+</style>
